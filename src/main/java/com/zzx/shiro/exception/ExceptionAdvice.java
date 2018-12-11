48f04eda5da8963840f69c58f906490a3b4e2ea5
John <1101648204@qq.com>
2018-12-11 22:27:13 +0800

    shiro异常统一处理机制

--- src/main/java/com/zzx/shiro/exception/ExceptionAdvice.java
+++ src/main/java/com/zzx/shiro/exception/ExceptionAdvice.java
@@ -1,7 +1,28 @@
 package com.zzx.shiro.exception;
 
+import com.zzx.shiro.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.AuthorizationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.transform.Result;
+
 /**
  * Created by Mr.John on 2018/12/11 21:51.
  **/
-public class ExceptionAdvice {
+@Slf4j
+@ControllerAdvice
+public class ExceptionAdvice extends Exception{
+
+    @ExceptionHandler({AuthorizationException.class})
+    @ResponseBody
+    public Object handleException(Exception e){
+//        e.printStackTrace();
+        log.info("--------------AuthorizationException--------------");
+        return 0;
+    }
+
 }
